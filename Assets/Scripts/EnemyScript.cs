605e6f687ea97d1fb069981f29811e882c338f8c
aidan <aiqu0691@colorado.edu>
2020-10-16 00:07:54 -0600

    New

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EnemyScript : MonoBehaviour
+{
+    public GameObject target;
+    private SpriteRenderer spriteRender;
+    public float enemyHealth = 3f;
+    public Sprite damage1;
+    public Sprite damage2;
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+    public float speed;
+    public float stoppingDistance;
+    public float retreatDistance;
+    public GameObject enemyProjectile;
+
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRender = GetComponent<SpriteRenderer>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        timeBtwShots = startTimeBtwShots;
+
+    }
+
+    private void Update()
+    {
+        Vector2 lookTarget = target.transform.position - transform.position;
+        if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+        else if (Vector2.Distance(transform.position, player.position) < stoppingDistance &&
+                 Vector2.Distance(transform.position, player.position) > retreatDistance)
+        {
+            transform.position = this.transform.position;
+        }
+        else if (Vector2.Distance(transform.position, player.position) < retreatDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+
+        if (timeBtwShots <= 0)
+        {
+            Instantiate(enemyProjectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Projectile")
+        {
+            if (enemyHealth == 1)
+            {
+                Destroy(this.gameObject);
+            }
+            else if (enemyHealth == 2)
+            {
+                spriteRender.sprite = damage2;
+            }
+            else if (enemyHealth == 3)
+            {
+                spriteRender.sprite = damage1;
+            }
+
+            enemyHealth--;
+        }
+
+    }
+}
+
+
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.UI;
+using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+
+public class PlayerScript : MonoBehaviour {
+    public SpriteRenderer spriteRenderer;
+    public Sprite newSprite;
+    public Sprite oldSprite;
+
+    private float     xPos;
+    private float     yPos;
+    public float      speed = .05f;
+    public float      leftWall, rightWall;
+    public float      bottomWall;
+    public float topWall;
+    public float health = 0.5f;
+    private Transform aimTrans;
+    private Transform endBarrel;
+    public GameObject projectile;
+    public KeyCode fireKey;
+    private Animator cannonAnim;
+    private Animator balloonDef;
+    private bool damagedState = false;
+    public event EventHandler<onShootArgs> OnShoot;
+
+    public class onShootArgs : EventArgs
+    {
+        public Vector3 muzzlePos;
+        public Vector3 shootPos;
+    }
+
+    private void Awake()
+    {
+        balloonDef = this.GetComponent<Animator>();
+        aimTrans = transform.Find("Aim");
+        cannonAnim = aimTrans.GetComponent<Animator>();
+        endBarrel = aimTrans.Find("endBarrel");
+    }
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 aimDir = (mousePos - transform.position).normalized;
+        float angle = Mathf.Atan2(aimDir.y, aimDir.x) * Mathf.Rad2Deg; 
+        aimTrans.eulerAngles = new Vector3(0,0,angle);
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            if (xPos > leftWall) {
+                xPos -= speed;
+                
+            }
+        }
+
+        if (yPos < bottomWall)
+        {
+            
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            if (xPos < rightWall) {
+                xPos += speed;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if (yPos > bottomWall)
+            {
+                balloonDef.SetTrigger("DownPress");
+                balloonDef.SetBool("Holding", true);
+                transform.localPosition = new Vector3(xPos, transform.position.y - speed*2, 0);
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            balloonDef.SetBool("Holding", false);
+        }
+        if (Input.GetMouseButtonDown(0))
+        
+        {
+            cannonAnim.SetTrigger("Shoot");
+            Instantiate(projectile,  endBarrel.position, Quaternion.Euler(0, 0, angle));
+        }
+
+        if (Input.GetKey(KeyCode.X) && damagedState == true)
+        {
+            spriteRenderer.sprite = oldSprite;
+            damagedState = false;
+
+        }
+
+        transform.localPosition = new Vector3(xPos, transform.position.y + speed, 0);
+        if (health <= 0)
+        {
+            damagedState = true;
+            spriteRenderer.sprite = newSprite;
+           while (damagedState == true)
+           { 
+               transform.localPosition= new Vector3(xPos, transform.position.y - speed, 0);
+            }
+            
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.tag == "EnemyProjectile")
+        {
+            Destroy(other.gameObject);
+            health -= .1f;
+            
+        }
+        
+    }
+
+}
+
+
+
+
+
+
+
+
